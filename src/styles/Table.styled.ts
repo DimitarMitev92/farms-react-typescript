b42d709c0ccc7c08a0264e9c6b4ed5ab01ff891b
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-05 19:38:33 +0200

    Add animation to tables

--- src/styles/Table.styled.ts
+++ src/styles/Table.styled.ts
@@ -1,12 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { mainColors } from "../static/mainColors";
 
 const primaryColor = mainColors.primaryColor;
 const hoverTextColor = mainColors.hoverTextColor;
 const hoverColor = mainColors.hoverColor;
 
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 export const TableContainer = styled.div`
   margin: 2em;
+  animation: ${fadeInUp} 0.5s ease-in-out;
 `;
 
 export const Table = styled.table`
